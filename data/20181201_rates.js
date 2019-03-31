0bd61f4451d616c586bf9396387699e01fa9c169
That_Guy <eghove@gmail.com>
2019-03-31 17:13:12 -0500

    scaffolding the first data object

---
+++ data/20181201_rates.js
@@ -0,0 +1,151 @@
+// 2018-12-01 rates object
+
+const rates_2018_12_01 = {
+  // no deps allowed for 10% and 20%
+  "10%" : 140.05,
+  "20%" : 276.84,
+  // 30% rates
+  "30%" : {
+    "single" : 428.83,
+    "spouse_only": 479.83,
+    "spouse_one_parent": 520.83,
+    "spouse_two_parent": 561.83,
+    "one_parent" : 469.83,
+    "two_parents" : 510.83,
+    "spouse_aa_allowance" : 47.00,
+    "spouse_one_child" : 516.83,
+    "child_only" : 462.83,
+    "spouse_one_parent_child" : 557.83,
+    "spouse_two_parents_child" : 598.83,
+    "one_parent_child" : 503.83,
+    "two_parents_child" : 544.83,
+    "additional_minor_child" : 25.00,
+    "additional_school_child" : 82.00
+  },
+  // 40% rates
+  "40%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  },
+  // 50% rates
+  "50%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  },
+  // 60% rates
+  "60%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  },
+  // 70% rates
+  "70%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  },
+  // 80% rates
+  "80%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  },
+  // 90% rates
+  "90%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  },
+  // 100% rates
+  "100%" : {
+    "single" : 0,
+    "spouse_only": 0,
+    "spouse_one_parent": 0,
+    "spouse_two_parent": 0,
+    "one_parent" : 0,
+    "two_parents" : 0,
+    "spouse_aa_allowance" : 0,
+    "spouse_one_child" : 0,
+    "child_only" : 0,
+    "spouse_one_parent_child" : 0,
+    "spouse_two_parents_child" : 0,
+    "one_parent_child" : 0,
+    "two_parents_child" : 0,
+    "additional_minor_child" : 0,
+    "additional_school_child" : 0
+  } 
+}

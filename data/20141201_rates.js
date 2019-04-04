6fd9b550c41ac2b5660bb1e4477746a1a60694ad
That_Guy <eghove@gmail.com>
2019-04-04 17:28:18 -0500

    adding 2014/2015 rates

---
+++ data/20141201_rates.js
@@ -0,0 +1,151 @@
+// 2014-12-01 rates object (2015-12-01 is the same)
+
+const rates_2014_12_01 = {
+  // no deps allowed for 10% and 20%
+  "10%" : 133.17,
+  "20%" : 263.23,
+  // 30% rates
+  "30%" : {
+    "single" : 407.75,
+    "spouse_only": 455.75,
+    "spouse_one_parent": 494.75,
+    "spouse_two_parent": 533.74,
+    "one_parent" : 446.75,
+    "two_parents" : 485.75,
+    "spouse_aa_allowance" : 44.00,
+    "spouse_one_child" : 491.75,
+    "child_only" : 439.75,
+    "spouse_one_parent_child" : 530.75,
+    "spouse_two_parents_child" : 569.75,
+    "one_parent_child" : 478.75,
+    "two_parents_child" : 517.75,
+    "additional_minor_child" : 24.00,
+    "additional_school_child" : 78.00
+  },
+  // 40% rates
+  "40%" : {
+    "single" : 587.36,
+    "spouse_only": 651.36,
+    "spouse_one_parent": 703.36,
+    "spouse_two_parent": 755.36,
+    "one_parent" : 639.36,
+    "two_parents" : 691.36,
+    "spouse_aa_allowance" : 59.00,
+    "spouse_one_child" : 699.36,
+    "child_only" : 630.36,
+    "spouse_one_parent_child" : 751.36,
+    "spouse_two_parents_child" : 803.36,
+    "one_parent_child" : 682.36,
+    "two_parents_child" : 734.36,
+    "additional_minor_child" : 32.00,
+    "additional_school_child" : 104.00
+  },
+  // 50% rates
+  "50%" : {
+    "single" : 836.13,
+    "spouse_only": 917.13,
+    "spouse_one_parent": 982.13,
+    "spouse_two_parent": 1047.13,
+    "one_parent" : 901.13,
+    "two_parents" : 966.13,
+    "spouse_aa_allowance" : 74.00,
+    "spouse_one_child" : 976.13,
+    "child_only" : 890.13,
+    "spouse_one_parent_child" : 1041.13,
+    "spouse_two_parents_child" : 1106.13,
+    "one_parent_child" : 955.13,
+    "two_parents_child" : 1020.13,
+    "additional_minor_child" : 40.00,
+    "additional_school_child" : 130.00
+  },
+  // 60% rates
+  "60%" : {
+    "single" : 1059.09,
+    "spouse_only": 1156.09,
+    "spouse_one_parent": 1234.09,
+    "spouse_two_parent": 1312.09,
+    "one_parent" : 1137.09,
+    "two_parents" : 1215.09,
+    "spouse_aa_allowance" : 89.00,
+    "spouse_one_child" : 1227.09,
+    "child_only" : 1124.09,
+    "spouse_one_parent_child" : 1305.09,
+    "spouse_two_parents_child" : 1383.09,
+    "one_parent_child" : 1202.09,
+    "two_parents_child" : 1280.09,
+    "additional_minor_child" : 48.00,
+    "additional_school_child" : 156.00
+  },
+  // 70% rates
+  "70%" : {
+    "single" : 1334.71,
+    "spouse_only": 1447.71,
+    "spouse_one_parent": 1538.71,
+    "spouse_two_parent": 1629.71,
+    "one_parent" : 1425.71,
+    "two_parents" : 1516.71,
+    "spouse_aa_allowance" : 104.00,
+    "spouse_one_child" : 1530.71,
+    "child_only" : 1409.71,
+    "spouse_one_parent_child" : 1621.71,
+    "spouse_two_parents_child" : 1712.71,
+    "one_parent_child" : 1500.71,
+    "two_parents_child" : 1591.71,
+    "additional_minor_child" : 56.00,
+    "additional_school_child" : 182.00
+  },
+  // 80% rates
+  "80%" : {
+    "single" : 1551.48,
+    "spouse_only": 1680.48,
+    "spouse_one_parent": 1784.48,
+    "spouse_two_parent": 1888.48,
+    "one_parent" : 1655.48,
+    "two_parents" : 1759.48,
+    "spouse_aa_allowance" : 118.00,
+    "spouse_one_child" : 1175.48,
+    "child_only" : 1637.48,
+    "spouse_one_parent_child" : 1879.48,
+    "spouse_two_parents_child" : 1983.48,
+    "one_parent_child" : 1741.48,
+    "two_parents_child" : 1845.48,
+    "additional_minor_child" : 64.00,
+    "additional_school_child" : 208.00
+  },
+  // 90% rates
+  "90%" : {
+    "single" : 1743.48,
+    "spouse_only": 1888.48,
+    "spouse_one_parent": 2005.48,
+    "spouse_two_parent": 2122.48,
+    "one_parent" : 1860.48,
+    "two_parents" : 1977.48,
+    "spouse_aa_allowance" : 133.00,
+    "spouse_one_child" : 1995.48,
+    "child_only" : 1840.48,
+    "spouse_one_parent_child" : 2112.48,
+    "spouse_two_parents_child" : 2229.48,
+    "one_parent_child" : 1957.48,
+    "two_parents_child" : 2074.48,
+    "additional_minor_child" : 72.00,
+    "additional_school_child" : 234.00
+  },
+  // 100% rates
+  "100%" : {
+    "single" : 2906.83,
+    "spouse_only": 3068.90,
+    "spouse_one_parent": 3198.96,
+    "spouse_two_parent": 3329.02,
+    "one_parent" : 3036.89,
+    "two_parents" : 3166.95,
+    "spouse_aa_allowance" : 148.64,
+    "spouse_one_child" : 3187.60,
+    "child_only" : 3015.22,
+    "spouse_one_parent_child" : 3317.66,
+    "spouse_two_parents_child" : 3447.72,
+    "one_parent_child" : 3145.28,
+    "two_parents_child" : 3275.48,
+    "additional_minor_child" : 80.52,
+    "additional_school_child" : 260.13
+  } 
+}

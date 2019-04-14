b7ba943a535d5c0c12fbd84b83bf5d331256ee2a
That_Guy <eghove@gmail.com>
2019-04-14 18:49:22 -0500

    starting to build the inputs component

--- va-comp-est/src/App.js
+++ va-comp-est/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Jumbotron from './components/Jumbotron'
+import Jumbotron from './components/Jumbotron';
+import Inputs from './components/Inputs';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,8 @@ class App extends Component {
     return (
       <div className="container">
         <Jumbotron />
+        <Inputs />
+
         
       </div>
     );
---
+++ va-comp-est/src/components/Inputs/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+
+function Inputs() {
+  return (
+    <div className="row">
+      <div className="col-md-6 offset-md-3 input-box">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Inputs;

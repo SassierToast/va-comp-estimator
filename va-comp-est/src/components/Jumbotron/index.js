15032bf52196d245085a2ae05630a9152db5aee4
That_Guy <eghove@gmail.com>
2019-04-14 18:35:41 -0500

    building the jumbotron component

---
+++ va-comp-est/src/components/Jumbotron/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './index.css';
+
+function Jumbotron () {
+  return (
+    <div className = "jumbotron">
+      <h1 className = "display-4">
+        Welcome to the VA Disability Compensation Estimator
+      </h1>
+      <p className = "lead">
+        Use this tool to estimate your monthly VA Disability Compensation benefits based on your combined evaluation for compensation.
+      </p>
+    </div>
+  )
+}
+
+export default Jumbotron;

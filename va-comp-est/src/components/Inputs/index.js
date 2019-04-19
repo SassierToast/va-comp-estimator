e89bcc89e24b89bccfe2e47e142dc4fa01403296
That_Guy <eghove@gmail.com>
2019-04-19 17:13:52 -0500

    progressing

--- va-comp-est/src/components/Inputs/index.js
+++ va-comp-est/src/components/Inputs/index.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
-
-function Inputs() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 offset-md-3 input-box">
-          <div className="form-group">
-            {/* <label for="exampleFormControlSelect1"></label> */}
-            <select className="form-control" id="effective-date">
-            <option value="" selected disabled hidden>Effective Date</option>
-              <option value="20181201">12/01/2018</option>
-              <option value="20171201">12/01/2017</option>
-              <option value="20161201">12/01/2016</option>
-              <option value="20141201">12/01/2015</option>
-              <option value="20141201">12/01/2014</option>
-            </select>
+// need to make this a stateful component so responses get saved.
+class Inputs extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 input-box">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <select className="form-control" id="effective-date">
+                      <option value="" selected disabled hidden>Effective Date</option>
+                      <option value="20181201">12/01/2018</option>
+                      <option value="20171201">12/01/2017</option>
+                      <option value="20161201">12/01/2016</option>
+                      <option value="20141201">12/01/2015</option>
+                      <option value="20141201">12/01/2014</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-
-
         </div>
-
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Inputs;

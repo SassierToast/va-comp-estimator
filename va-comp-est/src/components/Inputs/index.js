8d5e1cb2217b238e189c6a740f290417198ba6f3
That_Guy <eghove@gmail.com>
2019-04-18 20:03:33 -0500

    starting to build input

--- va-comp-est/src/components/Inputs/index.js
+++ va-comp-est/src/components/Inputs/index.js
@@ -4,11 +4,25 @@ import './index.css';
 
 function Inputs() {
   return (
-    <div className="row">
-      <div className="col-md-6 offset-md-3 input-box">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 input-box">
+          <div className="form-group">
+            {/* <label for="exampleFormControlSelect1"></label> */}
+            <select className="form-control" id="effective-date">
+            <option value="" selected disabled hidden>Effective Date</option>
+              <option value="20181201">12/01/2018</option>
+              <option value="20171201">12/01/2017</option>
+              <option value="20161201">12/01/2016</option>
+              <option value="20141201">12/01/2015</option>
+              <option value="20141201">12/01/2014</option>
+            </select>
+          </div>
+
+
+        </div>
 
       </div>
-
     </div>
   )
 }

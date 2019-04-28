05e28eb9d32a2773baba0d8964f964d90cd9b6b3
That_Guy <eghove@gmail.com>
2019-04-28 13:02:13 -0500

    state pulled from both toggles now

--- va-comp-est/src/components/Inputs/index.js
+++ va-comp-est/src/components/Inputs/index.js
@@ -6,18 +6,22 @@ import './index.css';
 class Inputs extends Component {
 
   state = {
-    effectiveDate: "",
+    effectiveDate: 0,
     maritalStatus: 0,
     spAidandAttendance: 0,
-    depParents: 0
+    depParents: 0,
+    depChildren18: 0,
+    depChildrenSchool: 0
   };
 
   componentDidMount() {
     this.setState({
-      effectiveDate: 0,
-      maritalStatus: 1,
-      spAidandAttendance: 0,
-      depParents: 0
+      effectiveDate: "",
+      maritalStatus: "single",
+      spAidandAttendance: "no",
+      depParents: 0,
+      depChildren18: 0,
+      depChildrenSchool: 0
     });
   };
 
@@ -27,6 +31,40 @@ class Inputs extends Component {
       () => console.log(this.state)
     );
   };
+
+  // this function handles the marital toggle
+  handleMaritalButton = () => {
+    let currentMx = this.state.maritalStatus;
+    if (currentMx === "single") {
+      this.setState(
+        { maritalStatus: "married" },
+        () => console.log(this.state)
+      )
+    } else if (currentMx === "married") {
+      this.setState(
+        { maritalStatus: "single" },
+        () => console.log(this.state)
+      )
+    } else console.log("something went wrong");
+  }
+
+
+  // this function handles the spouse aid and attendance toggle
+  handleSpAAButton = () => {
+    let currentAA = this.state.spAidandAttendance;
+    if (currentAA === "no") {
+      this.setState(
+        { spAidandAttendance: "yes" },
+        () => console.log(this.state)
+      )
+    } else if (currentAA === "yes") {
+      this.setState(
+        { spAidandAttendance: "no" },
+        () => console.log(this.state)
+      )
+    } else console.log("something went wrong");
+  }
+
 
   render() {
     return (
@@ -39,8 +77,9 @@ class Inputs extends Component {
                 <div className="col-md-12">
                   <div className="form-group">
                     <select className="form-control" id="effective-date"
-                     onChange={this.handleChange}
-                     >
+                      name="effectiveDate"
+                      onChange={this.handleChange}
+                    >
                       <option value="" selected disabled hidden>Effective Date</option>
                       <option value="20181201">12/01/2018</option>
                       <option value="20171201">12/01/2017</option>
@@ -56,14 +95,20 @@ class Inputs extends Component {
                 <div className="col-md-6 text-center">
                   <p>Single/Married</p>
                   <div className="custom-control custom-switch">
-                    <input type="checkbox" className="custom-control-input" id="maritalStatus" value={this.state.maritalStatus} name="maritalStatus" onChange={this.handleChange} />
+                    <input type="checkbox" className="custom-control-input" id="maritalStatus"
+                      name="maritalStatus"
+                      onChange={this.handleMaritalButton}
+                    />
                     <label className="custom-control-label" for="maritalStatus"></label>
                   </div>
                 </div>
                 <div className="col-md-6 text-center">
                   <p>Spouse Aid and Attendance</p>
                   <div className="custom-control custom-switch">
-                    <input type="checkbox" className="custom-control-input" id="spAA" />
+                    <input type="checkbox" className="custom-control-input" id="spAA"
+                      name="spAidandAttendance"
+                      onChange={this.handleSpAAButton}
+                    />
                     <label className="custom-control-label" for="spAA"></label>
                   </div>
                 </div>
@@ -92,8 +137,11 @@ class Inputs extends Component {
                   <p>Children Under 18</p>
                   <form>
                     <div className="col-auto my-1">
-                      <label className="mr-sm-2 sr-only" for="depChildren18">Dependent Children Under 18</label>
-                      <select className="custom-select mr-sm-2" id="depChildren18">
+                      <label className="mr-sm-2 sr-only" htmlFor="depChildren18">Dependent Children Under 18</label>
+                      <select className="custom-select mr-sm-2" id="depChildren18"
+                        name="depChildren18"
+                        onChange={this.handleChange}
+                      >
                         <option selected value="0">0</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -111,7 +159,10 @@ class Inputs extends Component {
                   <form>
                     <div className="col-auto my-1">
                       <label className="mr-sm-2 sr-only" for="depChildrenSchool">Dependent Children Under 18</label>
-                      <select className="custom-select mr-sm-2" id="depChildrenSchool">
+                      <select className="custom-select mr-sm-2" id="depChildrenSchool"
+                        name="depChildrenSchool"
+                        onChange={this.handleChange}
+                      >
                         <option selected value="0">0</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
